dadbef1015124e0e70ba06680156259c17422852
rfujioka <rodrigofujioka@gmail.com>
2024-09-12 04:53:34 -0300

    Atualização com o banco e libs

--- src/main/java/br/edu/unifacisa/live/controller/ProdutoController.java
+++ src/main/java/br/edu/unifacisa/live/controller/ProdutoController.java
@@ -3,12 +3,14 @@ package br.edu.unifacisa.live.controller;
 import br.edu.unifacisa.live.model.Produto;
 import br.edu.unifacisa.live.service.ProdutoService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/produtos")
